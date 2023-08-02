cdd4f033acd9f793502134f7fca667684567641c
BG <44068819+LeeBG@users.noreply.github.com>
2023-08-02 20:00:01 +0900

    [unrated] Title: 배열 조각하기, Time: 1.81 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int[] arr, int[] query) {
-        int[] answer = new int[query.length];
-        int index = 0;
-        for(int i : query){
-            answer[index] = arr[i];
-            index++;
-        }
+    public Integer[] solution(int[] arr, int[] query) {
+    
+    	ArrayList<Integer>  list = new ArrayList<Integer>();
+    	
+    	for(int i=0;i<arr.length;i++) {
+    		list.add(arr[i]);
+    	}
+    	// arr에서 잘라낼 기준 index를 담고 있는 query
+    	for(int i=0;i<query.length;i++) {
+    		if(i % 2== 0) {
+    			for(int j = list.size()-1;j>=query[i]+1;j--) {
+    				list.remove(list.size()-1);
+    			}
+    		}else {
+    			for(int j=0;j<query[i];j++) {
+    				list.remove(0);
+    			}
+    		}
+    	}
+        
+    	Integer[] answer = list.toArray(new Integer[list.size()]);
+    	
         return answer;
     }
 }
