07d1ea868cea5910a461944c052db0cad86eae52
BG <44068819+LeeBG@users.noreply.github.com>
2023-08-12 23:49:53 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 7.25 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+
+class Solution {
+    public Integer[] solution(String today, String[] terms, String[] privacies) {
+    	
+        int[][] expires = new int [privacies.length][3];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+       loop: for(int i=0;i<privacies.length;i++){
+            for(String t : terms){
+                if(privacies[i].contains(t.split(" ")[0])){
+                    expires[i][0] = Integer.parseInt(privacies[i].split(" ")[0].split("\\.")[0]);
+                    expires[i][1] = Integer.parseInt(privacies[i].split(" ")[0].split("\\.")[1])+Integer.parseInt(t.split(" ")[1]);
+                    expires[i][2] = Integer.parseInt(privacies[i].split(" ")[0].split("\\.")[2]);
+                     
+                    System.out.println(expires[i][0]);
+                    System.out.println(expires[i][1]);
+                    System.out.println(expires[i][2]);
+                    
+                    if(expires[i][1] > 12){
+                    	int add = expires[i][1]/12;
+                    	if(expires[i][1]%12 == 0) {
+                    		add--;
+                    		expires[i][1] = 12;
+                    	}else {
+                    		expires[i][1] %= 12;
+                    	}
+                        expires[i][0] += add;
+                    }
+                    
+                    for(int k=0;k<3;k++) {
+                    	if(Integer.parseInt(today.split("\\.")[k])<expires[i][k]) {
+                    		continue loop;
+                    	}else if(Integer.parseInt(today.split("\\.")[k]) == expires[i][k]) {
+                    		if(k == 2) {
+                    			list.add(i+1);
+                    		}
+                    	}else {
+                    		list.add(i+1);
+                    		continue loop;
+                    	}
+                    }
+                }
+            }//inner for
+        }//ext for
+ 
+       Integer[] answer = list.toArray(new Integer[list.size()]);
+        
+        return answer;
+    }
+}
