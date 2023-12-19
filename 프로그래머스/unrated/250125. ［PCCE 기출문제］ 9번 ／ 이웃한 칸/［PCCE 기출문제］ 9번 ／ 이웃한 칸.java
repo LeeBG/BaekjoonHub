63b364c81d1b8a3388cc2f478240216b7855d3ec
BG <44068819+LeeBG@users.noreply.github.com>
2023-12-19 16:47:35 +0900

    [level unrated] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,23 +1,6 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
-        int[][] direction = {{0,-1},{0,1},{-1,0},{1,0}}; // 상하좌우
-        
-        String color = board[h][w];
-        
-        for(int i=0;i<direction.length;i++) {
-            int mov_x = h + direction[i][0];
-            int mov_y = w + direction[i][1];
-            
-            if((mov_x >= 0) && (mov_y >= 0)&& (mov_x < board.length) && (mov_y < board[0].length)){
-                if(color.equals(board[h+direction[i][0]][w+direction[i][1]])) {
-                    answer++;
-                }    
-            }
-            
-           
-        }
-        
         return answer;
     }
 }
