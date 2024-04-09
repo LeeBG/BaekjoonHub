4dd583b9e3c830456c33cd0e03d0e8709cc723e9
BG <44068819+LeeBG@users.noreply.github.com>
2024-04-09 17:20:12 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.51 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,28 +1,11 @@
-import java.util.Arrays;
-import java.util.*;
 class Solution
 {
     public int solution(int []A, int []B)
     {
         int answer = 0;
 
-        Integer[] B2 = new Integer[B.length];
-
-        for(int i=0;i<B2.length;i++){
-            B2[i] = B[i];
-        }
-
-        Arrays.sort(A);
-        Arrays.sort(B2, new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
-
-        for(int i=0;i<A.length;i++){
-            answer += A[i]*B2[i];
-        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
