aac54c58eb060d1f37643b481888a4bd194badf1
BG <44068819+LeeBG@users.noreply.github.com>
2024-04-12 17:50:15 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,14 @@
 SELECT COUNT(id) as COUNT
 FROM ECOLI_DATA
 where (GENOTYPE & 2 != 2) and (GENOTYPE & 4 = 4 or GENOTYPE & 1 = 1)
+
+/*
+
+4321
+----
+1000     & (AND 연산) 0010  0000 - x
+1111     & (AND 연산) 0010  0010 - x
+0001     & (AND 연산) 0001  만족 - o
+1101     & (AND 연산) 0100  만족 - o
+
+*/
