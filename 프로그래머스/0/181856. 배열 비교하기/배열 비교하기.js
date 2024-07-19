951d0518d1c55877d001c05f59bbe067bd5d44ba
BG <44068819+LeeBG@users.noreply.github.com>
2024-07-19 16:50:19 +0900

    [level 0] Title: 배열 비교하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,23 @@
+function solution(arr1, arr2) {
+    var answer = 0;
+    let sum1 = 0;
+    let sum2 = 0;
+    if(arr1.length < arr2.length){
+        answer = -1;
+    }else if(arr1.length > arr2.length){
+        answer = 1;
+    }else{
+        for(let i=0;i<arr1.length;i++){
+            sum1 += arr1[i];
+            sum2 += arr2[i];
+        }
+        if(sum1 < sum2) {
+            answer = -1;
+        }else if(sum2 < sum1) {
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+    }
+    return answer;
+}
